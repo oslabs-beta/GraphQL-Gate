7f5f6551e265b10a0d8cb1324af08f43ca92b9a9
[Evan McNeely] <evanmcneely@me.com>
2022-05-19 08:50:25 -0400

    started to layout the test structure for buildTypeWeight function

--- src/analysis/buildTypeWeights.ts
+++ src/analysis/buildTypeWeights.ts
@@ -1,4 +1,4 @@
-import { GraphQLSchemaConfig } from 'graphql/type/schema';
+import { GraphQLSchema } from 'graphql/type/schema';
 
 interface Fields {
     [index: string]: number;
@@ -13,7 +13,7 @@ interface TypeWeightObject {
     [index: string]: Type;
 }
 
-function buildTypeWeightsFromSchema(schema: GraphQLSchemaConfig): TypeWeightObject {
+function buildTypeWeightsFromSchema(schema: GraphQLSchema): TypeWeightObject {
     throw Error(`getTypeWeightsFromSchema is not implemented.`);
 }
 export default buildTypeWeightsFromSchema;
--- test/analysis/buildTypeWeights.test.ts
+++ test/analysis/buildTypeWeights.test.ts
@@ -1,1 +1,13 @@
+import { buildSchema } from 'graphql';
+import { GraphQLSchema } from 'graphql/type/schema';
 import buildTypeWeightsFromSchema from '../../src/analysis/buildTypeWeights';
+
+describe('Test buildTypeWeightsFromSchema function', () => {
+    beforeEach(() => {
+        let schema: GraphQLSchema;
+    });
+
+    test('creates the type weight object from graphql schema object', () => {});
+
+    test('');
+});
