7b055881ae1cd6bb11277c7b493ffe919a23d3c8
[Evan McNeely] <evanmcneely@me.com>
2022-05-19 22:09:02 -0400

    finished writing an overview of implementation for buildTypeWeightsFromSchema function

--- src/analysis/buildTypeWeights.ts
+++ src/analysis/buildTypeWeights.ts
@@ -1,9 +1,16 @@
 import { GraphQLSchema } from 'graphql/type/schema';
 
 /**
- * The default type weights object is based off of Shopifys implewentation of query
+ * The default typeWeightsConfig object is based off of Shopifys implementation of query
  * cost analysis. Our function should input a users configuration of type weights or fall
- * back on shopifys on
+ * back on shopifys settings. We can change this later.
+ *
+ * This function should
+ *  - itreate through the schema object and create the typeWeightObject as described in the tests
+ *  - validate that the typeWeightsConfig parameter has no negative values (throw an error if it does)
+ *
+ * @param schema
+ * @param typeWeightsConfig
  */
 function buildTypeWeightsFromSchema(
     schema: GraphQLSchema,
