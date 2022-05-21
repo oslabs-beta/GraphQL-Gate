f94394025051c59e360aa2bfaafea18df19ae824
Stephan Halarewicz <shalarewicz@gmail.com>
2022-05-21 16:08:52 -0400

    added redis client connection options

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,9 +1,8 @@
-import { RedisClientType } from 'redis';
+import { RedisClientOptions } from 'redis';
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import { GraphQLSchema } from 'graphql/type/schema';
 import { defaultTypeWeightsConfig } from '../analysis/buildTypeWeights';
 
-// FIXME: Should redisClient have the option of an under the hood implementation (not provided by user)
 // FIXME: Will the developer be responsible for first parsing the schema from a file?
 // Can consider accepting a string representing a the filepath to a schema
 // FIXME: Should a 429 status be sent by default or do we allow the user to handle blocked requests?
@@ -12,7 +11,7 @@ import { defaultTypeWeightsConfig } from '../analysis/buildTypeWeights';
  * Primary entry point for adding GraphQL Rate Limiting middleware to an Express Server
  * @param {RateLimiterSelection} rateLimiter Specify rate limiting algorithm to be used
  * @param {GraphQLSchema} schema GraphQLSchema object
- * @param {RedisClientType} redisClient redis client used for caching user request informaiton
+ * @param {RedisClientOptions} redisClientOptions valid node-redis connection options. See https://github.com/redis/node-redis/blob/HEAD/docs/client-configuration.md
  * @param {TypeWeightConfig} typeWeightConfig Optional type weight configuration for the GraphQL Schema.
  * Defaults to {mutation: 10, object: 1, field: 0, connection: 2}
  * @returns {RequestHandler} express middleware that computes the complexity of req.query and calls the next middleware
@@ -22,17 +21,17 @@ import { defaultTypeWeightsConfig } from '../analysis/buildTypeWeights';
 export function expressRateLimiter(
     rateLimiter: RateLimiterSelection,
     schema: GraphQLSchema,
-    redisClient: RedisClientType,
+    redisClientOptions: RedisClientOptions,
     typeWeightConfig: TypeWeightConfig = defaultTypeWeightsConfig
 ): RequestHandler {
     // TODO: Parse the schema to create a TypeWeightObject. Throw ValidationError if schema is invalid
+    // TODO: Connect to Redis store using provided options. Default to localhost:6379
     // TODO: Configure the selected RateLimtier
     // TODO: Configure the complexity analysis algorithm to run for incoming requests
 
     const middleware: RequestHandler = (req: Request, res: Response, next: NextFunction) => {
         // TODO: Parse query from req.query, compute complexity and pass necessary info to rate limiter
         // TODO: Call next if query is successful, send 429 status if query blocked, call next(err) with any thrown errors
-
         next(Error('Express rate limiting middleware not implemented'));
     };
     return middleware;
