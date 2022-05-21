cd0f1c1c66f94e0a443b5a71df30bbf2f25acdb3
Stephan Halarewicz <shalarewicz@gmail.com>
2022-05-21 13:23:26 -0400

    separated allowed/blocked token bucket tests for clarity

--- test/rateLimiters/tokenBucket.test.ts
+++ test/rateLimiters/tokenBucket.test.ts
@@ -42,69 +42,85 @@ xdescribe('Test TokenBucket Rate Limiter', () => {
     });
 
     describe('TokenBucket returns correct number of tokens and updates redis store as expected', () => {
-        test('after an ALLOWED request', async () => {
-            // Bucket intially full
-            const withdraw5 = 5;
-            expect((await limiter.processRequest(user1, timestamp, withdraw5)).tokens).toBe(
-                CAPACITY - withdraw5
-            );
-            const tokenCountFull = await getBucketFromClient(client, user1);
-            expect(tokenCountFull).toBe(CAPACITY - withdraw5);
+        describe('after an ALLOWED request...', () => {
+            test('bucket is initially full', async () => {
+                // Bucket intially full
+                const withdraw5 = 5;
+                expect((await limiter.processRequest(user1, timestamp, withdraw5)).tokens).toBe(
+                    CAPACITY - withdraw5
+                );
+                const tokenCountFull = await getBucketFromClient(client, user1);
+                expect(tokenCountFull).toBe(CAPACITY - withdraw5);
+            });
 
-            // Bucket partially full but enough time has elapsed to fill the bucket since the last request and
-            // has leftover tokens after reqeust
-            const initial = 6;
-            const partialWithdraw = 1;
-            await setTokenCountInClient(client, user2, initial, timestamp);
-            expect(
-                (
-                    await limiter.processRequest(
-                        user2,
-                        timestamp + 1000 * (CAPACITY - initial),
-                        initial + partialWithdraw
-                    )
-                ).tokens
-            ).toBe(CAPACITY - (initial + partialWithdraw));
-            const tokenCountPartial = await getBucketFromClient(client, user2);
-            expect(tokenCountPartial).toBe(CAPACITY - (initial + partialWithdraw));
+            test('bucket is partially full and request has leftover tokens', async () => {
+                // Bucket partially full but enough time has elapsed to fill the bucket since the last request and
+                // has leftover tokens after reqeust
+                const initial = 6;
+                const partialWithdraw = 1;
+                await setTokenCountInClient(client, user2, initial, timestamp);
+                expect(
+                    (
+                        await limiter.processRequest(
+                            user2,
+                            timestamp + 1000 * (CAPACITY - initial),
+                            initial + partialWithdraw
+                        )
+                    ).tokens
+                ).toBe(CAPACITY - (initial + partialWithdraw));
+                const tokenCountPartial = await getBucketFromClient(client, user2);
+                expect(tokenCountPartial).toBe(CAPACITY - (initial + partialWithdraw));
+            });
 
             // Bucket partially full and no leftover tokens after reqeust
-            const initial2 = 6;
-            await setTokenCountInClient(client, user2, initial, timestamp);
-            expect((await limiter.processRequest(user2, timestamp, initial2)).tokens).toBe(0);
-            const tokenCountPartialToEmpty = await getBucketFromClient(client, user2);
-            expect(tokenCountPartialToEmpty).toBe(0);
+            test('bucket is partially full and request has no leftover tokens', async () => {
+                const initial = 6;
+                await setTokenCountInClient(client, user2, initial, timestamp);
+                expect((await limiter.processRequest(user2, timestamp, initial)).tokens).toBe(0);
+                const tokenCountPartialToEmpty = await getBucketFromClient(client, user2);
+                expect(tokenCountPartialToEmpty).toBe(0);
 
-            // Bucket initially empty but enough time elapsed to paritally fill bucket since last request
-            await setTokenCountInClient(client, user4, 0, timestamp);
-            expect((await limiter.processRequest(user4, timestamp + 6000, 4)).tokens).toBe(2);
-            const count = await getBucketFromClient(client, user4);
-            expect(count).toBe(2);
+                // Bucket initially empty but enough time elapsed to paritally fill bucket since last request
+                await setTokenCountInClient(client, user4, 0, timestamp);
+                expect((await limiter.processRequest(user4, timestamp + 6000, 4)).tokens).toBe(2);
+                const count = await getBucketFromClient(client, user4);
+                expect(count).toBe(2);
+            });
         });
 
-        test('after a BLOCKED request', async () => {
+        describe('after a BLOCKED request...', () => {
             let redisData: RedisBucket;
-            // Initial request greater than capacity
-            expect((await limiter.processRequest(user1, timestamp, CAPACITY + 1)).tokens).toBe(
-                CAPACITY
-            );
 
-            redisData = await getBucketFromClient(client, user1);
-            expect(redisData.tokens).toBe(CAPACITY);
+            test('where intial request is greater than bucket capacity', async () => {
+                // Initial request greater than capacity
+                expect((await limiter.processRequest(user1, timestamp, CAPACITY + 1)).tokens).toBe(
+                    CAPACITY
+                );
 
-            // Bucket is partially full and time has elapsed but not enough to allow the current request
-            const fillLevel = 5;
-            const timeDelta = 3;
-            const requestedTokens = 9;
-            await setTokenCountInClient(client, user2, fillLevel, timestamp);
+                redisData = await getBucketFromClient(client, user1);
+                expect(redisData.tokens).toBe(CAPACITY);
+            });
 
-            expect(
-                (await limiter.processRequest(user1, timestamp + timeDelta * 1000, requestedTokens))
-                    .tokens
-            ).toBe(fillLevel + timeDelta * REFILL_RATE);
+            test('Bucket is partially full but not enough time elapsed to complete the request', async () => {
+                // Bucket is partially full and time has elapsed but not enough to allow the current request
+                const fillLevel = 5;
+                const timeDelta = 3;
+                const requestedTokens = 9;
+                await setTokenCountInClient(client, user2, fillLevel, timestamp);
 
-            redisData = await getBucketFromClient(client, user2);
-            expect(redisData.tokens).toBe(fillLevel + timeDelta * REFILL_RATE);
+                expect(
+                    (
+                        await limiter.processRequest(
+                            user1,
+                            timestamp + timeDelta * 1000,
+                            requestedTokens
+                        )
+                    ).tokens
+                ).toBe(fillLevel + timeDelta * REFILL_RATE);
+
+                redisData = await getBucketFromClient(client, user2);
+                expect(redisData.tokens).toBe(fillLevel + timeDelta * REFILL_RATE);
+            });
         });
     });
 
