606823faf59866b36b14941642fe520c425ab006
Jon Dewey <donjewey@gmail.com>
2022-07-09 21:51:31 -0700

    pseudocode converted to completed test skeleton

--- test/rateLimiters/slidingWindowCounter.test.ts
+++ test/rateLimiters/slidingWindowCounter.test.ts
@@ -16,7 +16,7 @@ const user2 = '2';
 const user3 = '3';
 const user4 = '4';
 
-async function getBucketFromClient(redisClient: ioredis.Redis, uuid: string): Promise<RedisWindow> {
+async function getWindowFromClient(redisClient: ioredis.Redis, uuid: string): Promise<RedisWindow> {
     const res = await redisClient.get(uuid);
     // if no uuid is found, return -1 for all values, which is impossible
     if (res === null) return { currentTokens: -1, previousTokens: -1, fixedWindowStart: -1 };
@@ -45,19 +45,6 @@ describe('Test TokenBucket Rate Limiter', () => {
         timestamp = new Date().valueOf();
     });
 
-    // AFTER AN ALLOWED REQUEST
-    // sliding window can take two requests within capacity
-    // sliding window can take more than capacity when new minute elapses
-    // sliding window is initially full, but after a minute passes allows more requests
-    // sliding window allows requests under allowed limit set by formula
-    // 3 rolling window tests with different proportions (.25, .5, .75)
-
-    // AFTER A BLOCKED REQUEST
-    // initial request is greater than capacity
-    // window is partially full but not enough time elapsed to reach new window
-    // window blocks requests over allowed limit set by formula
-    // 3 rolling window tests with different proportions (.25, .5, .75)
-
     describe('SlidingWindowCounter returns correct number of tokens and updates redis store as expected', () => {
         describe('after an ALLOWED request...', () => {
             afterEach(() => {
@@ -69,7 +56,7 @@ describe('Test TokenBucket Rate Limiter', () => {
                 expect((await limiter.processRequest(user1, timestamp, withdraw5)).tokens).toBe(
                     CAPACITY - withdraw5
                 );
-                const tokenCountFull = await getBucketFromClient(client, user1);
+                const tokenCountFull = await getWindowFromClient(client, user1);
                 expect(tokenCountFull.currentTokens).toBe(CAPACITY - withdraw5);
             });
 
@@ -87,26 +74,41 @@ describe('Test TokenBucket Rate Limiter', () => {
                         )
                     ).tokens
                 ).toBe(CAPACITY - (initial + partialWithdraw));
-                const tokenCountPartial = await getBucketFromClient(client, user2);
+                const tokenCountPartial = await getWindowFromClient(client, user2);
                 expect(tokenCountPartial.currentTokens).toBe(
                     CAPACITY - (initial + partialWithdraw)
                 );
             });
 
-            // Bucket partially full and no leftover tokens after reqeust
-            xtest('bucket is partially full and request has no leftover tokens', async () => {
+            // window partially full and no leftover tokens after request
+            xtest('fixed window is partially full and request has no leftover tokens', async () => {
                 const initial = 6;
                 // await setTokenCountInClient(client, user2, initial, timestamp);
                 expect((await limiter.processRequest(user2, timestamp, initial)).tokens).toBe(0);
-                const tokenCountPartialToEmpty = await getBucketFromClient(client, user2);
+                const tokenCountPartialToEmpty = await getWindowFromClient(client, user2);
+                expect(tokenCountPartialToEmpty.currentTokens).toBe(0);
+            });
+
+            xtest('fixed window can process two requests within capacity', async () => {
+                const initial = 6;
+                // await setTokenCountInClient(client, user2, initial, timestamp);
+                expect((await limiter.processRequest(user2, timestamp, initial)).tokens).toBe(0);
+                const tokenCountPartialToEmpty = await getWindowFromClient(client, user2);
                 expect(tokenCountPartialToEmpty.currentTokens).toBe(0);
             });
 
             // Bucket initially empty but enough time elapsed to paritally fill bucket since last request
-            xtest('bucket is initially empty but enough time has elapsed to partially fill the bucket', async () => {
+            xtest('fixed window is initially full but after new fixed window is initialized request is allowed', async () => {
                 // await setTokenCountInClient(client, user4, 0, timestamp);
                 expect((await limiter.processRequest(user4, timestamp + 6000, 4)).tokens).toBe(2);
-                const count = await getBucketFromClient(client, user4);
+                const count = await getWindowFromClient(client, user4);
+                expect(count.currentTokens).toBe(2);
+            });
+
+            xtest('sliding window allows requests under allowed limit set by formula', async () => {
+                // three different tests within, with different rolling window proportions (.25, .5, .75)
+                expect((await limiter.processRequest(user4, timestamp + 6000, 4)).tokens).toBe(2);
+                const count = await getWindowFromClient(client, user4);
                 expect(count.currentTokens).toBe(2);
             });
         });
@@ -115,25 +117,52 @@ describe('Test TokenBucket Rate Limiter', () => {
             afterEach(() => {
                 client.flushall();
             });
+
+            xtest('initial request is greater than capacity', () => {});
+
+            xtest('window is partially full but not enough time elapsed to reach new window', () => {});
+
+            xtest('window blocks requests over allowed limit set by formula', () => {
+                // 3 rolling window tests with different proportions (.25, .5, .75)
+            });
         });
     });
 
-    // allows user to consume current allotment of tokens
-    // blocks exceeding requests over token allotment
-    // sliding window never exceeds maximum capacity
-    // rolling window formula operates as expected
-    // fixed window and current/previous tokens update as expected
-    // sliding window allows custom window sizes
-    // sliding window allows custom capacities
-    // users have their own windows
-    // sliding window doesn't allow capacity/window size < 1
-    // all windows should be able to be reset
+    describe('SlidingWindowCounter functions as expected', () => {
+        afterEach(() => {
+            client.flushall();
+        });
 
-    describe('SlidingWindowCounter functions as expected', () => {});
+        xtest('allows user to consume current allotment of tokens', () => {});
 
-    // timestamp correctly updated in redis
-    // current/previous tokens correctly updated in redis
-    // all windows should be able to be reset
+        xtest('blocks exceeding requests over token allotment', () => {});
 
-    describe('SlidingWindowCounter correctly updates Redis cache', () => {});
+        xtest('sliding window never exceeds maximum capacity', () => {});
+
+        xtest('rolling window formula operates as expected', () => {});
+
+        xtest('fixed window and current/previous tokens update as expected', () => {});
+
+        xtest('sliding window allows custom window sizes', () => {});
+
+        xtest('sliding window allows custom capacities', () => {});
+
+        xtest('users have their own windows', () => {});
+
+        xtest("sliding window doesn't allow capacity/window size < 1", () => {});
+
+        xtest('all windows should be able to be reset', () => {});
+    });
+
+    describe('SlidingWindowCounter correctly updates Redis cache', () => {
+        afterEach(() => {
+            client.flushall();
+        });
+
+        xtest('timestamp correctly updated in redis', () => {});
+
+        xtest('current/previous tokens correctly updated in redis', () => {});
+
+        xtest('all windows should be able to be reset', () => {});
+    });
 });
